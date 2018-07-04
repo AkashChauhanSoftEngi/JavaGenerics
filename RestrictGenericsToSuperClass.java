eac14b33ff6619e9681109dc5377ba2a0d1554ce
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-07-04 23:11:09 +0530

    Add files via upload

---
+++ RestrictGenericsToSuperClass.java
@@ -0,0 +1,19 @@
+package com.project.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestrictGenericsToSuperClass {
+	public static void main(String[] args) {
+		List<Number> list = new ArrayList<Number>();
+		RestrictGenericsToSuperClass.addNumbers(list);
+		System.out.println(list.toString());
+	}
+	
+	/*list of any type that is a super type of Integer*/
+	public static void addNumbers(List<? super Integer> list) {
+	    for (int i = 1; i <= 10; i++) {
+	        list.add(i);
+	    }
+	}
+}
